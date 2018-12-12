0151d3b9ba480e638762de579ead7f86378f297b
DimitrisBo <31184597+DimitrisBovasianos@users.noreply.github.com>
2018-12-12 14:00:36 +0200

    Update writer.py

--- writer.py
+++ writer.py
@@ -5,7 +5,7 @@ with open('file4.txt','r') as f:
     i = 0
     for word  in f:
         x = list(word)
-        lines.append([x[0],x[1],x[len(word)-3],x[len(word)-2],word])
+        lines.append([x[0],x[1],x[2],x[len(word)-3],x[len(word)-2],word])
 for word in lines:
     for i in range(0,len(word)-1):
         word[i] = ord(word[i])
